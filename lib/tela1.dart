4a12ba1459c6b2af97d45ac3f13dc2e5f819b03b
Laura Ellen de Souza Santos <laura.santos@gclaims.com.br>
2021-08-26 19:26:04 -0300

    sidebar and itens

--- lib/tela1.dart
+++ lib/tela1.dart
@@ -1,85 +1,90 @@
 import 'package:flutter/material.dart';
+import 'package:fastoncoming/tela2.dart';
+
 
 class TelaUm extends StatefulWidget {
   @override
   _TelaUmState createState() => _TelaUmState();
 }
 
+
 class _TelaUmState extends State<TelaUm> {
+
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromARGB(255, 192, 18, 16),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 40,
-            ),
-            Center(
-              child: Container(
-                height: 280,
-                width: 320,
-                color: Colors.grey,
-                child: Text("Logo aqui"),
-              ),
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            Center(
-              child: Container(
-                height: 80,
-                width: 80,
-                color: Colors.grey,
-                child: Text("Carregador aqui"),
-              ),
-            ),
-            SizedBox(
-              height: 80,
-            ),
-            SizedBox(
-              height: 40,
-              width: 100,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text(
-                  'Pt-Br',
-                  style: TextStyle(color: Colors.black, fontSize: 22),
-                ),
-                style: ButtonStyle(
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(18.0),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            SizedBox(
-              height: 40,
-              width: 100,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text(
-                  'En-Us',
-                  style: TextStyle(color: Colors.black, fontSize: 22),
-                ),
-                style: ButtonStyle(
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(18.0),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor : Color.fromARGB(255, 92, 198, 186)
       ),
+      home :  Scaffold(
+        appBar: AppBar(
+          title: Text("FastOnComing"),
+        ),
+        body : Container (
+          color: Colors.white,
+        ),
+        drawer: Drawer(
+          child: ListView(
+            children: <Widget>[
+              Container(
+                height: 50.0,
+                child: DrawerHeader(
+                  child: Text('Seja bem vindo!',
+                    style: TextStyle(fontSize: 16),
+                    ),
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 92, 198, 186),
+                  ),
+                ),
+              ),
+              ListTile( 
+                leading: Icon(Icons.account_circle),
+                title: Text("Perfil"),
+                trailing: Icon(Icons.arrow_forward),
+                onTap: () {
+                  Navigator.pop(context);
+                }
+              ),
+              ListTile(
+                leading: Icon(Icons.space_dashboard),
+                title: Text("Cartão de vacina"),
+                trailing: Icon(Icons.arrow_forward),
+                onTap: () {
+                  Navigator.of(context).pop();
+                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => TelaDois()));
+                },
+               ),
+              ListTile(
+                leading: Icon(Icons.announcement),
+                title: Text("Campanha de vacinação"),
+                trailing: Icon(Icons.arrow_forward),
+                onTap: () {
+                  Navigator.of(context).pop();
+                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => TelaDois()));
+                },
+              ),
+              ListTile( 
+                leading: Icon(Icons.bloodtype),
+                title: Text("Doe sangue!"),
+                trailing: Icon(Icons.arrow_forward),
+                onTap: () {
+                  Navigator.pop(context);
+                }
+              ),
+              ListTile( 
+                leading: Icon(Icons.local_hospital),
+                title: Text("Hospitais/UBS próximos"),
+                trailing: Icon(Icons.arrow_forward),
+                onTap: () {
+                  Navigator.pop(context);
+                }
+              )
+            ],
+           )
+        )
+      )
     );
   }
 }
